ab0b801b40249c0ed6cbf84ff7af40bbdd693358
stevegustason <steve.gustason@gmail.com>
2023-04-08 08:27:44 -0500

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Steven Gustason on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
